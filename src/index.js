7a4c3df79cb61e84b00ce41a43954d463ce0893a
RomanKhorol <Rom4uk691@gmail.com>
2023-01-10 17:00:29 +0200

    modul_10_firstStep

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 import './css/styles.css';
-
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  inputField: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  oneCountry: document.querySelector('.country-info'),
+};
+refs.inputField.addEventListener(
+  'input',
+  debounce(onInputChange, DEBOUNCE_DELAY)
+);
+function onInputChange(e) {
+  let dataCounry = refs.inputField.value.trim();
+  if (dataCounry === '') {
+    return;
+  }
+  fetchCountries(dataCounry).then(renderCoutries);
+}
+function renderCoutries(countries) {
+  console.log(countries.length);
+  if (countries.length > 10) {
+    refs.countryList.style.display = 'none';
+    refs.oneCountry.style.listStyleType = 'none';
+    Notiflix.Notify.warning(
+      `Too many matches found. Please enter a more specific name.`
+    );
+  } else if (countries.length > 1 && countries.length < 10) {
+    refs.oneCountry.style.display = 'none';
+    const countryItem = countries.map(country => {
+      return `<li class="country-item"><p>${country.name.official}</p>
+    <img src=${country.flags.svg} alt=flag width = 50px />
+  </li>`;
+    });
+    refs.countryList.innerHTML = countryItem;
+    refs.countryList.style.display = 'flex';
+    refs.countryList.style.flexDirection = 'column';
+    refs.countryList.style.marginTop = '0px';
+    refs.countryList.style.paddingLeft = '0px';
+  } else if (countries.length === 1) {
+    refs.countryList.style.display = 'none';
+    refs.oneCountry.style.display = 'flex';
+    refs.oneCountry.style.listStyleType = 'none';
+    const item = countries.map(country => {
+      return `<li class="country-item"><p>${country.name.official}</p>
+    <img src=${country.flags.svg} alt=flag width = 50px />
+    <p>Capital: ${country.capital}</p>
+    <p>Populations: ${country.populatoin}</p>
+    <p>Languages: ${country.official}</p>
+  </li>`;
+    });
+    refs.oneCountry.innerHTML = item;
+  }
+}
