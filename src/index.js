32c7163fbfeb64c7697301b1d6ee52ba3b070753
RomanKhorol <Rom4uk691@gmail.com>
2023-01-12 14:31:23 +0200

    modul_10_done

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ refs.inputField.addEventListener(
 function onInputChange(e) {
   let dataCounry = refs.inputField.value.trim();
   if (dataCounry === '') {
+    refs.countryList.style.display = 'none';
+    refs.oneCountry.style.display = 'none';
     return;
   }
   fetchCountries(dataCounry).then(renderCoutries);
@@ -23,34 +25,40 @@ function renderCoutries(countries) {
   console.log(countries.length);
   if (countries.length > 10) {
     refs.countryList.style.display = 'none';
+    refs.oneCountry.style.display = 'none';
     refs.oneCountry.style.listStyleType = 'none';
     Notiflix.Notify.warning(
       `Too many matches found. Please enter a more specific name.`
     );
   } else if (countries.length > 1 && countries.length < 10) {
     refs.oneCountry.style.display = 'none';
-    const countryItem = countries.map(country => {
-      return `<li class="country-item"><p>${country.name.official}</p>
-    <img src=${country.flags.svg} alt=flag width = 50px />
+    const countryItem = countries
+      .map(country => {
+        return `<li class="country-item"><img src=${country.flags.svg} alt=flag width = 60px /><p>${country.name.official}</p>
+    
   </li>`;
-    });
+      })
+      .join('');
     refs.countryList.innerHTML = countryItem;
     refs.countryList.style.display = 'flex';
     refs.countryList.style.flexDirection = 'column';
     refs.countryList.style.marginTop = '0px';
     refs.countryList.style.paddingLeft = '0px';
+    refs.countryList.style.gap = '5px';
   } else if (countries.length === 1) {
     refs.countryList.style.display = 'none';
     refs.oneCountry.style.display = 'flex';
     refs.oneCountry.style.listStyleType = 'none';
     const item = countries.map(country => {
-      return `<li class="country-item"><p>${country.name.official}</p>
+      return `<li><p>${country.name.official}</p>
     <img src=${country.flags.svg} alt=flag width = 50px />
     <p>Capital: ${country.capital}</p>
-    <p>Populations: ${country.populatoin}</p>
-    <p>Languages: ${country.official}</p>
+    <p>Populations: ${country.population}</p>
+    <p>Languages: ${Object.values(country.languages).join(', ')}</p>
   </li>`;
     });
     refs.oneCountry.innerHTML = item;
+  } else {
+    Notiflix.Notify.warning(`Oops, there is no country with that name`);
   }
 }
